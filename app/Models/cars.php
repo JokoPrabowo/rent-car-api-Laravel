426317f06d5562a9e66401771a893ce17c5547e4
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-11-04 20:37:06 +0700

    update models

--- app/Models/cars.php
+++ app/Models/cars.php
@@ -3,9 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Cars extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'renter_id',
+        'brand',
+        'model',
+        'color',
+        'year',
+        'rental_price_per_day',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
     public function deals() {
         return $this->hasMany(Deals::class);
     }
